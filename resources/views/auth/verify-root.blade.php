e64fe3c789c8f8bc1cde47be793c510d18afdbc6
0roc4n <esparragojames@gmail.com>
2024-05-07 10:58:49 +0800

    verify root blade

--- resources/views/auth/verify-root.blade.php
+++ resources/views/auth/verify-root.blade.php
@@ -1,31 +1,37 @@
 <x-guest-layout>
-    <h1 class="mb-3 font-semibold text-center">To set up an account, log in using the root account.</h1>
-    <form method="POST" action="{{ route('verify-root-account') }}">
-        @csrf
-        <div>
-            <x-input-label for="email" :value="__('Root Account Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <div  class="flex justify-center items-center mb-10 mt-6">
+        <img src="{{ asset('assets/PHLPOSTLogo.png') }}" alt="PhilPostLogo" class="h-10 fill-current text-gray-800" /> 
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Root Account Password')" />
+    <div class="login-form-container flex justify-center mt-5"> <!-- Added mt-10 to add margin at the top -->
+        <div class="left w-full">
+                <form method="POST" action="{{ route('verify-root-account') }}">
+                    @csrf
+                    <h1 class="text-gray-500 text-sm font-semibold flex justify-center mb-4"> To set up an account, log in using the root account.</h1>
+                    <div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button class="w-full h-10 rounded-xl bg-secondary text-center hover:bg-secondary_hover transition-colors duration:200">
-                {{ __('Verify') }}
-            </x-primary-button>
+                    <div class="form-group relative mb-3 w-full">
+                        <x-text-input type="email" id="email" name="email" :value="old('email')" required autofocus autocomplete="username" class="block mt-1 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-400 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer h-10" placeholder=" " required/>
+                        <label for="email" :value="__('Root Account Email')" class="absolute text-xs text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Root Account Email</label> 
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
 
-            <section>
-                
-            </section>
+                    <div class="form-group relative mb-5">
+                        <x-text-input type="password" id="password" name="password" required autocomplete="current-password" class="block mt-1 text-dark form-control block px-3 pb-2.5 pt-3 text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-400 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer h-10 w-full" placeholder=" " required/>
+                        <label for="password" :value="__('Root Account Password')" class="absolute text-xs text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Root Account Password</label>
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+                    
+
+                    <div class="flex justify-center mt-5">
+                        <x-primary-button class="ms-3 bg-blue-700 hover:bg-blue-900" style="border-radius: 15px;">
+                            {{ __('Verify') }}
+                        </x-primary-button>
+                    </div>
+
+                </form>
         </div>
-    </form>
+    </div>
 </x-guest-layout>
